82cbd5a1956219575f2b44904e181573517fdbdc
nclhowell <nclhowell@gmail.com>
2023-02-24 10:59:45 -0800

    cms 3c

--- week3/cms/src/app/app.component.ts
+++ week3/cms/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Contacts';
+  loadedFeature = 'documents';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
 }
